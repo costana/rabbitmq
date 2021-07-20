d6977ad2eb78277780c101c0660d30aa31e3d4c7
Thiago Costa <thiago.costa@jet.com.br>
2021-07-20 07:17:53 -0300

    add exemplos de producer e consumer

---
+++ examples/Receive/Receive.cs
@@ -0,0 +1,49 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    public class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            factory.UserName = "tico";
+            factory.Password = "tico123";
+            factory.VirtualHost = "ticoVHost";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+
+            }
+
+        }
+
+    }
+}
